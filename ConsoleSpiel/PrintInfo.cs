de1d2dfc58fbceead91f4221b92da830b54d1441
Martyian <Ivan.Martyian@audius.de>
2024-10-22 16:36:34 +0200

    first commit

---
+++ ConsoleSpiel/PrintInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSpiel
+{
+    public class PrintInfo
+    {
+        public string TextToPrint { get; set; }
+        public ConsoleColor TextColor { get; set; }
+        public bool PrintLine { get; set; }
+        public PrintInfo(string textToPrint, ConsoleColor textColor, bool printLine)
+        {
+            TextToPrint = textToPrint;
+            TextColor = textColor;
+            PrintLine = printLine;
+        }
+    }
+}
